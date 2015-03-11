f24ddb3408963eaa51255b8fc30964805f63c309
Robert Peralta <robert.apb@hotmail.com>
2015-03-10 22:50:47 -0400

    Completed moving-the-cam

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,6 +18,7 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 mov = new Vector3(hAxis, 0f, vAxis);
+        
         GetComponent<Rigidbody>().AddForce(mov * speed * Time.deltaTime);
     }
 }
