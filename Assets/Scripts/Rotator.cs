24fa1b33a69d3a3c1cf792fb30e2400d99885337
Robert Peralta <robert.apb@hotmail.com>
2015-03-10 23:37:00 -0400

    + Completed create pick-ups

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    public float speed = 3f;
+
+	void Update () {
+
+
+        Vector3 rotation = new Vector3(Random.Range(10, 30), Random.Range(10, 30), Random.Range(10, 30));
+        GetComponent<Transform>().Rotate(rotation * speed * Time.deltaTime);
+	}
+}
