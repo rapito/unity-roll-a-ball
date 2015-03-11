799a666a0116bf886c9c9917d27017266d0dbf6c
Robert Peralta <robert.apb@hotmail.com>
2015-03-10 22:44:21 -0400

    Completed moving-the-player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 500f;
+    float hAxis;
+    float vAxis;
+
+
+    void Update()
+    {
+        hAxis = Input.GetAxis("Horizontal");
+        vAxis = Input.GetAxis("Vertical");
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 mov = new Vector3(hAxis, 0f, vAxis);
+        GetComponent<Rigidbody>().AddForce(mov * speed * Time.deltaTime);
+    }
+}
